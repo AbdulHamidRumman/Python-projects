b79e2308fd7f4d1adaaefac84513322979378057
AbdulHamidRumman <abdulhamidrumman@gmail.com>
2022-07-02 10:11:27 +0600

    updated code

--- Random_Grouping/random_grouping.py
+++ Random_Grouping/random_grouping.py
@@ -2,14 +2,13 @@ from os import system as sys
 from time import sleep
 import random
 
-runs = 30   # Number of iterations tells how many times will the code run
+start = int(input("Enter Starting Roll No.: "))
+stop = int(input("Enter Ending Roll No.: "))
+no_grp = int(input("Enter Number of Groups: "))
+section = input("Enter Section (A or B): ")         # 'A' or 'B'  Group Names will be like A1, A2, ... or B1, B2, ...
+runs = int(input("Enter Number of Iterations: "))   # Number of iterations tells how many times will the code run
 
 for r in range(runs):
-    start = 31
-    stop = 60
-    no_grp = 6
-    section = 'B' # 'A' or 'B'  Group Names will be like A1, A2, ... or B1, B2, ...
-
     std_lst = list(range(start,stop+1))
     no_per_grp = int(len(std_lst)/no_grp)
     random.shuffle(std_lst)
