ecec42514d98081feeabdd0b6cb95d2c06c31f87
AbdulHamidRumman <abdulhamidrumman@gmail.com>
2022-07-02 19:19:55 +0600

    fixed major bug

--- Random_Grouping/random_grouping.py
+++ Random_Grouping/random_grouping.py
@@ -8,13 +8,11 @@ no_grp = int(input("Enter Number of Groups: "))
 section = input("Enter Section (A or B): ")         # 'A' or 'B'  Group Names will be like A1, A2, ... or B1, B2, ...
 runs = int(input("Enter Number of Iterations: "))   # Number of iterations tells how many times will the code run
 
-std_lst = list(range(start,stop+1))
-no_per_grp = int(len(std_lst)/no_grp)
-
 for r in range(runs):
+    std_lst = list(range(start,stop+1))
+    no_per_grp = int(len(std_lst)/no_grp)
     random.shuffle(std_lst)
-    grps = {}
-
+    grps = {} 
     for i in range(no_grp):
         grp = random.sample(std_lst,no_per_grp)
         grps[section+str(i+1)] = grp
